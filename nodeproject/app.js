10d34f4fb755462d9684e7cb979608a589a313f5
Tyler Grubb <grubber1618@gmail.com>
2017-02-19 20:38:27 +0000

    finished lesson 6.3 - begin with ejs

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -4,13 +4,14 @@ var app = express();
 var port = process.env.PORT;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
 app.use(express.static('bower_components'));
 
-
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res){
-   res.send('Hello World!'); 
+   //res.send('Hello World!'); 
+   res.render('index', { list: ['first val', '2nd val', '3rd val'] });
 });
 
 app.get('/routing', function (req, res){
