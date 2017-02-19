f9ac072c5c76798c82c14821170d718ee96cdd37
Tyler Grubb <grubber1618@gmail.com>
2017-02-19 19:18:51 +0000

    finished lesson 4.3

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -3,6 +3,9 @@ var app = express();
 
 var port = process.env.PORT;
 
+app.use(express.static('public'));
+app.use(express.static('src/views'))
+
 app.get('/', function (req, res){
    res.send('Hello World!'); 
 });
@@ -13,4 +16,4 @@ app.get('/routing', function (req, res){
 
 app.listen(port, function (err) {
     console.log('The server is running on port: ' + port);
-})
+});
