97c669153f779d0c0366194cfa1186a37c512949
Tyler Grubb <grubber1618@gmail.com>
2017-02-19 19:10:15 +0000

    completed lesson 4.2

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,16 @@
-var hello = 'Hello variables!';
-console.log(hello);
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT;
+
+app.get('/', function (req, res){
+   res.send('Hello World!'); 
+});
+
+app.get('/routing', function (req, res){
+   res.send('Hello Routing!'); 
+});
+
+app.listen(port, function (err) {
+    console.log('The server is running on port: ' + port);
+})
