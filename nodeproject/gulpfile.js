4dc36ea6a3ebf2ac4434b9331e3d161a8eae154b
Tyler Grubb <grubber1618@gmail.com>
2017-02-19 20:03:26 +0000

    finished lesson 5.3 - inject dependencies with Wiredep

--- nodeproject/gulpfile.js
+++ nodeproject/gulpfile.js
@@ -7,3 +7,16 @@ gulp.task('style', function () {
     return gulp.src(jsFiles)
         .pipe(jshint());
 });
+
+gulp.task('inject', function () {
+    var wireDep = require('wiredep').stream;
+    var options = { 
+        bowerJson: require('./bower.json'), 
+        directory: './bower_components',
+        ignorePath: '../../bower_components'
+    }
+    
+    return gulp.src('./src/views/*.html')
+        .pipe(wireDep(options))
+        .pipe(gulp.dest('./src/views'));
+});
