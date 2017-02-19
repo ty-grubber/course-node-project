8c3b1221d52b0dba3789909b0b33c527e835fc3f
Tyler Grubb <grubber1618@gmail.com>
2017-02-19 20:13:12 +0000

    finished lesson 5.4 - injection with gulp-inject

--- nodeproject/gulpfile.js
+++ nodeproject/gulpfile.js
@@ -10,13 +10,22 @@ gulp.task('style', function () {
 
 gulp.task('inject', function () {
     var wireDep = require('wiredep').stream;
-    var options = { 
+    var inject = require('gulp-inject');
+    
+    var injectSrc = gulp.src(['./public/css/*.css', './public/js/*.js']);
+    
+    var injectOptions = {
+        ignorePath: '/public'
+    }
+    
+    var wireDepOptions = { 
         bowerJson: require('./bower.json'), 
         directory: './bower_components',
         ignorePath: '../../bower_components'
     }
     
     return gulp.src('./src/views/*.html')
-        .pipe(wireDep(options))
+        .pipe(wireDep(wireDepOptions))
+        .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
 });
