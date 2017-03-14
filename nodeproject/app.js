a2c44bb2755a48d69bcd7b1949b57268b3531ab4
Tyler Grubb <grubber1618@gmail.com>
2017-03-14 22:39:43 +0000

    finished lesson 6.4 - ejs menu; finished lesson 7.2 - add router using express

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT;
+var eventRouter = express.Router();
 
 app.use(express.static('public'));
 app.use(express.static('bower_components'));
@@ -9,9 +10,31 @@ app.use(express.static('bower_components'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+eventRouter.route('/')
+    .get(function(req, res) {
+        res.send('Hello Events!');
+    });
+    
+eventRouter.route('/event')     //handles /Events/event
+    .get(function(req, res) {
+        res.send('Hello Single Event!');
+    });
+    
+app.use('/Events', eventRouter);
+
 app.get('/', function (req, res){
    //res.send('Hello World!'); 
-   res.render('index', { list: ['first val', '2nd val', '3rd val'] });
+   res.render('index', { 
+       list: ['first val', '2nd val', '3rd val'], 
+       nav: [
+           {link: 'Services', text: 'Services'}, 
+           {link: 'Portfolio', text: 'Porfolio'},
+           {link: 'About', text: 'About'},
+           {link: 'Team', text: 'Team'},
+           {link: 'Contact', text: 'Contact'},
+           {link: 'Events', text: 'Events'}
+           ]
+   });
 });
 
 app.get('/routing', function (req, res){
